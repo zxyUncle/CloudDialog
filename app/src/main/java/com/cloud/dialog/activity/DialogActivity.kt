7e95d7a7c2a4896d6ea7aeb580dff12b883d8515
张行于 <>
2023-02-16 19:53:09 +0800

    倒计时优化

--- app/src/main/java/com/cloud/dialog/activity/DialogActivity.kt
+++ app/src/main/java/com/cloud/dialog/activity/DialogActivity.kt
@@ -107,7 +107,7 @@ class DialogActivity : ComponentActivity() , LifecycleObserver {
                 .setView(R.layout.dialog_test)//必选                         自定义布局的View
                 .setTransparency(0.2f)//可选                                  默认0.2f
                 .setCancelable(true) //可选                                   默认true
-                .isShowKeyboard(R.id.tvDialgContent)
+                .setEditFocus(R.id.tvDialgContent)
                 .setAnimator(AnimatorEnum.TRAN_T.VALUE)//可选，               默认AnimatorEnum.ZOOM.VALUE
                 .setOnClick(R.id.tvDialogConfirm) //可选  Dialog中的点击事件
                 .OnClickListener { view, alertDialogUtils -> //必选                    点击事件的回调
